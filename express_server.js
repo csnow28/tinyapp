ad270e76d9edb0189e19780e3170ff4dbee80c65
Camillia Snow <s.camillia@gmail.com>
2023-09-29 20:32:47 -0300

    adding code to display the short and long URLs

--- express_server.js
+++ express_server.js
@@ -2,12 +2,23 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
-app.setting("view engine", "ejs");
+app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:id", (req, res) => {
+  const longURL = urlDatabase[req.params.id];
+  const templateVars = { id: req.params.id, longURL: longURL };
+  res.render("urls_show", templateVars);
+});
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -18,6 +29,7 @@ app.get("/urls.json", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
